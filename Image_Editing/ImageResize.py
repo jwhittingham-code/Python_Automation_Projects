11e511f83ca8bfffc5382fbf242fe9d1fe9607d5
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-02 12:13:17 +1000

    refactor code and removed unneeded print statements

--- Image_Editing/ImageResize.py
+++ Image_Editing/ImageResize.py
@@ -1,13 +1,11 @@
 from PIL import Image
 import os
 
-target_width = 500
-target_height = 500
+#target resolution
+target_width,target_height = 500, 500
 
 path = './Image_Editing/imgs'
 pathout = './Image_Editing/editedImgs'
-
-print(os.listdir(path))
 
 for file in os.listdir(path):
     # open file and prepare clean name for save
@@ -16,6 +14,7 @@ for file in os.listdir(path):
     crop_img = image
     print(file)
 
+    # checks image ratio and crops image to square
     if image.width > image.height:
         print('wide image')
         crop_factor = (image.width - image.height) /2
@@ -26,10 +25,7 @@ for file in os.listdir(path):
         crop_img = image.crop((0,crop_factor,image.width ,image.height - crop_factor))
     else:
         print("square image")
-    
-    print(f'crop sides by {crop_factor} pixels')
-    
- 
+        
+    # resize and set all images to RGB
     edit = crop_img.resize((target_width, target_height)).convert("RGB")
-  
     edit.save(f'{pathout}/{clean_name}_edit.jpg')
