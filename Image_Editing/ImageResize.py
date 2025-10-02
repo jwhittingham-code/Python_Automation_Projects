bc45a81913a29327566bc43356fd576c9cfbf794
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-02 10:55:05 +1000

    Created loop that resizes images to target width and height. Next step is to check if images are not square and to crop them prior to resize if required.

--- Image_Editing/ImageResize.py
+++ Image_Editing/ImageResize.py
@@ -1,10 +1,16 @@
-import PIL
+from PIL import Image
 import os
 
 target_width = 500
-taget_height = 500
+target_height = 500
 
 path = './Image_Editing/imgs'
 pathout = './Image_Editing/editedImgs'
 
 print(os.listdir(path))
+
+for file in os.listdir(path):
+    image = Image.open(f'{path}/{file}')
+    edit = image.resize((target_width, target_height))
+    clean_name = os.path.splitext(file)[0]
+    edit.save(f'{pathout}/{clean_name}_edit.jpg')
