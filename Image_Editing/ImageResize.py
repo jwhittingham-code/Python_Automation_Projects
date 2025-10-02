79ea0457350a53a347f21082e9daba502e4641d7
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-02 11:28:52 +1000

    resize loop now compares the height to the width and if it is not square crops the image based on the smaller side before resizing and saving.

--- Image_Editing/ImageResize.py
+++ Image_Editing/ImageResize.py
@@ -10,7 +10,26 @@ pathout = './Image_Editing/editedImgs'
 print(os.listdir(path))
 
 for file in os.listdir(path):
+    # open file and prepare clean name for save
     image = Image.open(f'{path}/{file}')
-    edit = image.resize((target_width, target_height))
     clean_name = os.path.splitext(file)[0]
+    crop_img = image
+    print(file)
+
+    if image.width > image.height:
+        print('wide image')
+        crop_factor = (image.width - image.height) /2
+        crop_img = image.crop((crop_factor,0,image.width - crop_factor,image.height))
+    elif image.width < image.height:
+        print('tall image')
+        crop_factor = (image.height - image.width) /2
+        crop_img = image.crop((0,crop_factor,image.width ,image.height - crop_factor))
+    else:
+        print("square image")
+    
+    print(f'crop sides by {crop_factor} pixels')
+    
+ 
+    edit = crop_img.resize((target_width, target_height)).convert("RGB")
+  
     edit.save(f'{pathout}/{clean_name}_edit.jpg')
