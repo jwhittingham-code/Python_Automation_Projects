5953a5cdcecf70cf3d505ee416af013dc2e227aa
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-08 10:39:36 +1100

    Built functions that call API, organise and display json output

--- Web_Scrapping_API/Pokedex.py
+++ Web_Scrapping_API/Pokedex.py
@@ -1,8 +1,38 @@
 import requests, json
 
 baseUrl = 'https://pokeapi.co/api/v2/'
-name = 'pokemon/ditto'
 
-data = requests.get(f'{baseUrl}{name}')
-file = data.json()
-print(file)
+def pickPokemon():
+    nameOrId = input("pick a Pokemon by name or ID: ")
+    call = requests.get(f'{baseUrl}/pokemon/{nameOrId}')
+    #print(call)
+    try:
+        pokemon = call.json()
+        if pokemon:
+            return pokemon 
+        else:
+            return "Failed to find Pokemon"
+    except:
+        return "Failed to call Pokemon API"
+
+def types(pokemon):
+    typesfile = pokemon["types"]
+    types = list()
+   
+    for item in typesfile:
+        typeitem = item["type"]
+        types.append(typeitem["name"])
+    return types
+
+def displayPokemon(pokemon):
+    try:
+        print('Name: ',pokemon['name'].upper())
+        print('ID: ', pokemon['id'])
+        typing = ', '.join(types(pokemon))
+        print("Types: ",typing)       
+    except:
+        print("failed")
+
+critter = pickPokemon()
+displayPokemon(critter)
+
