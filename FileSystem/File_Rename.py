0cbc965f4ec4221950658257a1ebff95a2654061
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-07 11:06:24 +1100

    Finished rename script - useful for reformatting file names in bulk

--- FileSystem/File_Rename.py
+++ FileSystem/File_Rename.py
@@ -1,7 +1,16 @@
-import os
+import os, re
 
 path = './FileSystem/files'
 
 print(os.listdir(path))
-os.rename('./FileSystem/files/file1.txt','./FileSystem/files/file_1.txt')
+#os.rename('./FileSystem/files/file1.txt','./FileSystem/files/file_1.txt')
+
+for file in os.listdir(path):
+    number = re.findall("[0-9]+",file)[0]
+    filetype = file.rsplit(".")[1]
+    name = file.rsplit(number[0])[0]
+    newfilepath = f'{path}/{name}_{number}.{filetype}'
+    print(newfilepath)
+    os.rename(f'{path}/{file}', newfilepath)
+
 print(os.listdir(path))
