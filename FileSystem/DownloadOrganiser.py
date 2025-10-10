f08e523b918e3f049f15acc199f76db7f9619be2
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-10 15:58:41 +1100

    setting destination folders so I can set this up to not repeat itself

--- FileSystem/DownloadOrganiser.py
+++ FileSystem/DownloadOrganiser.py
@@ -2,20 +2,43 @@ import os, pathlib, shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+src = pathlib.Path("/home/j/Downloads")
+
+imageFolder = "img"
+MusicFolder = 'music'
+VideoFolder = 'video'
+ebookFolder = 'books'
+
+
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if not event.is_directory:
             print(f"file {os.path.basename(event.src_path)} has been modified!")
-            print(event.src_path)
-            print(os.path.basename(event.src_path))
+         
             file = os.path.basename(event.src_path)
+            fileFormat = file.split(".")[1]
+            print(fileFormat)
             print(file)
-            if file.endswith('.txt'):
-                shutil.move(event.src_path,f"/home/j/Downloads/text/{file}" )
-                print("file moved")
-            elif file.endswith('.jpg'):
-                shutil.move(event.src_path,f"/home/j/Downloads/img/{file}" )
-                print("file moved")
+
+            destinationFolder = ''
+
+            match fileFormat:
+                case 'jpg':
+                    destinationFolder = imageFolder
+                case 'mp4':
+                    destinationFolder = VideoFolder
+                case 'mp3':
+                    destinationFolder = MusicFolder
+                case 'pdf':
+                    destinationFolder = ebookFolder
+
+
+            # if file.endswith('.txt'):
+            #     shutil.move(event.src_path,f"/home/j/Downloads/text/{file}" )
+            #     print("file moved")
+            # elif file.endswith('.jpg'):
+            #     shutil.move(event.src_path,f"/home/j/Downloads/img/{file}" )
+            #     print("file moved")
         return super().on_modified(event)
 
 src = pathlib.Path("/home/j/Downloads")
