07a287fa300260dd8cec684df34596ce23375f02
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-10 15:26:31 +1100

    got core function working - in rough state, requires refactor

--- FileSystem/DownloadOrganiser.py
+++ FileSystem/DownloadOrganiser.py
@@ -1,21 +1,37 @@
-import os
+import os, pathlib, shutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        # if (not event.is_directory) and event.src_path.endswith(".txt"):
-        print(f"file {event.src_path} has been modified!")
-        # return super().on_modified(event)
+        if not event.is_directory:
+            print(f"file {os.path.basename(event.src_path)} has been modified!")
+            print(event.src_path)
+            print(os.path.basename(event.src_path))
+            file = os.path.basename(event.src_path)
+            print(file)
+            if file.endswith('.txt'):
+                shutil.move(event.src_path,f"/home/j/Downloads/text/{file}" )
+                print("file moved")
+            elif file.endswith('.jpg'):
+                shutil.move(event.src_path,f"/home/j/Downloads/img/{file}" )
+                print("file moved")
+        return super().on_modified(event)
 
-
-
+src = pathlib.Path("/home/j/Downloads")
+itemlist = list()
 # for i in os.scandir(src):
-#     print(i)
+#     #print(i)
+#     itemlist.append(i)
+
+print(itemlist)
+
 
 observer = Observer()
 event_handler = MyHandler()
-src = "/home/j/Downloads"
+
+
+    
 observer.schedule(event_handler,src,recursive=True)
 
 observer.start()
