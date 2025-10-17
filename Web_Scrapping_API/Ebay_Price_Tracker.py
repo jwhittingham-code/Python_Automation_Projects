84ca5676c3ee4563ad38855089836fde018b7ec1
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-17 23:16:18 +1100

    Script now returns a list of all items and their price.

--- Web_Scrapping_API/Ebay_Price_Tracker.py
+++ Web_Scrapping_API/Ebay_Price_Tracker.py
@@ -1,4 +1,5 @@
 import bs4 as bs
+import re 
 #removed some imported items Im not sure I needed. 
 
 from selenium import webdriver
@@ -11,11 +12,11 @@ from selenium.webdriver.support import expected_conditions as EC
 link = "https://www.ebay.com.au/sch/i.html?_nkw=zelda+ds&_sacat=0&_from=R40&_trksid=p4624852.m570.l1313"
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get(link)
 #setting title to see whats loaded
-title = driver.title
-print("driver title: ", title)
+# title = driver.title
+# print("driver title: ", title)
 
 #waiting for page to load so we can grab information. 
 wait = WebDriverWait(driver, 30)
@@ -23,14 +24,38 @@ wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME,"srp-results")))
 
 #grabbing page source, closing page then parsing source html      
 body = driver.page_source
+title = driver.title
 driver.quit()
 page_parse = bs.BeautifulSoup(body, 'html.parser')
 
 #Setting the title a 2nd time as a method to check if the expected HTML has loaded
-title = driver.title
+
 print("driver title: ", title)
 
 #currently just testing one piece of data before building out rest of app. 
-results = page_parse.find("span",{"class":"su-styled-text primary bold large-1 s-card__price"})
-print(results)
+results = page_parse.find("ul",{"class":"srp-results srp-list clearfix"})
+
+for li in results:
+    try:
+        item_title = li.find("span", {"class":"su-styled-text primary default"})
+        item_title = str(item_title)
+        item_title = re.split(r'[><]',item_title)      
+    except:
+        pass
+    
+    try:
+        item_price = li.find("span", {"class":"su-styled-text primary bold large-1 s-card__price"})
+        item_price = str(item_price)
+        item_price = re.split(r'[><]',item_price)   
+    except:
+        pass
+
+    try:
+        print(item_title[2])
+        print(item_price[2])
+
+    except:
+        pass
+
+
 
