5802f06f66441a3a8c1f5b8b030bc74b8740de35
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-21 10:48:25 +1100

    Added item links to print out.

--- Web_Scrapping_API/Ebay_Price_Tracker.py
+++ Web_Scrapping_API/Ebay_Price_Tracker.py
@@ -15,8 +15,6 @@ link = "https://www.ebay.com.au/sch/i.html?_nkw=zelda+ds&_sacat=0&_from=R40&_trk
 driver = webdriver.Firefox()
 driver.get(link)
 #setting title to see whats loaded
-# title = driver.title
-# print("driver title: ", title)
 
 #waiting for page to load so we can grab information. 
 wait = WebDriverWait(driver, 30)
@@ -38,21 +36,32 @@ results = page_parse.find("ul",{"class":"srp-results srp-list clearfix"})
 for li in results:
     try:
         item_title = li.find("span", {"class":"su-styled-text primary default"})
-        item_title = str(item_title)
-        item_title = re.split(r'[><]',item_title)      
+        item_title = re.split(r'[><]',str(item_title))  
+        item_title = item_title[2]    
     except:
         pass
     
     try:
         item_price = li.find("span", {"class":"su-styled-text primary bold large-1 s-card__price"})
-        item_price = str(item_price)
-        item_price = re.split(r'[><]',item_price)   
+        item_price = re.split(r'[><]',str(item_price)) 
+        item_price = item_price[2]
+         
     except:
         pass
 
     try:
-        print(item_title[2])
-        print(item_price[2])
+        item_link = li.find("a", {"class":"su-link"})
+        item_link = re.split(r'[><]',str(item_link))  
+        item_link = item_link[1].split('"')
+        item_link = item_link[41]
+    except:
+        pass
+
+    try:
+        print(item_title)
+        print(item_price)
+        print(item_link)
+        # print("list length", len(item_link))
 
     except:
         pass
