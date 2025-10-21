f4874fec694eef0262415c206eb02c735734ee36
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-21 11:27:11 +1100

    Refactored code to add items to list as a function. created function to pull 5 cheapest items.

--- Web_Scrapping_API/Ebay_Price_Tracker.py
+++ Web_Scrapping_API/Ebay_Price_Tracker.py
@@ -7,7 +7,61 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def additem(results):
+    ilist = []
+    for li in results:
+        item = {}
+        try:
+            item_title = li.find("span", {"class":"su-styled-text primary default"})
+            item_title = re.split(r'[><]',str(item_title))  
+            item_title = item_title[2]    
+        except:
+            pass
+        
+        try:
+            item_price = li.find("span", {"class":"su-styled-text primary bold large-1 s-card__price"})
+            item_price = re.split(r'[><]',str(item_price)) 
+            item_price = item_price[2].split(" ")
+            item_price[1] = float(item_price[1].strip("$"))
 
+            #NOTE: creating a list for price [0] is currency [1] is value
+            
+        except:
+            pass
+
+        try:
+            item_link = li.find("a", {"class":"su-link"})
+            item_link = re.split(r'[><]',str(item_link))  
+            item_link = item_link[1].split('"')
+            item_link = item_link[41]
+        except:
+            pass
+
+        try:
+            item["title"] = item_title
+            item["price"] = item_price
+            item["link"] = item_link
+            
+            if item["price"] != ['None']:
+                ilist.append(item)
+        except:
+            pass
+    return ilist
+
+def listcheapest(ilist):
+    top5 = []
+    for item in ilist:
+        
+        if len(top5) < 5:
+            top5.append(item)
+        elif len(top5) <= 5:
+            for i in range(5):
+                if item["price"] < top5[i]["price"]:
+                    top5.pop(i)
+                    top5.append(item)
+                    break
+                continue
+    return top5
 
 link = "https://www.ebay.com.au/sch/i.html?_nkw=zelda+ds&_sacat=0&_from=R40&_trksid=p4624852.m570.l1313"
 
@@ -33,38 +87,10 @@ print("driver title: ", title)
 #currently just testing one piece of data before building out rest of app. 
 results = page_parse.find("ul",{"class":"srp-results srp-list clearfix"})
 
-for li in results:
-    try:
-        item_title = li.find("span", {"class":"su-styled-text primary default"})
-        item_title = re.split(r'[><]',str(item_title))  
-        item_title = item_title[2]    
-    except:
-        pass
-    
-    try:
-        item_price = li.find("span", {"class":"su-styled-text primary bold large-1 s-card__price"})
-        item_price = re.split(r'[><]',str(item_price)) 
-        item_price = item_price[2]
-         
-    except:
-        pass
+itemlist = listcheapest(additem(results))
 
-    try:
-        item_link = li.find("a", {"class":"su-link"})
-        item_link = re.split(r'[><]',str(item_link))  
-        item_link = item_link[1].split('"')
-        item_link = item_link[41]
-    except:
-        pass
+print(itemlist)
 
-    try:
-        print(item_title)
-        print(item_price)
-        print(item_link)
-        # print("list length", len(item_link))
-
-    except:
-        pass
 
 
 
