bdb71b4323d9305a19c663446bce8f53a2adc6bf
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-21 11:52:42 +1100

    Add function to display results. refactored to make more readable

--- Web_Scrapping_API/Ebay_Price_Tracker.py
+++ Web_Scrapping_API/Ebay_Price_Tracker.py
@@ -7,6 +7,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#--------------------------------------------------------------------------
+#functions
+#--------------------------------------------------------------------------
+def roundprice(price):
+    i = int(round(price))
+    return i 
+
 def additem(results):
     ilist = []
     for li in results:
@@ -61,14 +68,25 @@ def listcheapest(ilist):
                     top5.append(item)
                     break
                 continue
+    
+    top5 = sorted(top5, key = lambda x: int(round(x["price"][1])))
     return top5
 
-link = "https://www.ebay.com.au/sch/i.html?_nkw=zelda+ds&_sacat=0&_from=R40&_trksid=p4624852.m570.l1313"
+def displaylist(ilist):
+    for i in ilist:
+        print("")
+        print(i["title"])
+        print(i["price"][0]," $",i["price"][1])
+        print("link: ", i["link"])
+        print("")
+        print("-----------------------------------------------------------------------------------")
 
+#--------------------------------------------------------------------------
+#setting link and creating scrapping driver from selenium
+link = "https://www.ebay.com.au/sch/i.html?_nkw=zelda+ds&_sacat=0&_from=R40&_trksid=p4624852.m570.l1313"
 
 driver = webdriver.Firefox()
 driver.get(link)
-#setting title to see whats loaded
 
 #waiting for page to load so we can grab information. 
 wait = WebDriverWait(driver, 30)
@@ -80,16 +98,15 @@ title = driver.title
 driver.quit()
 page_parse = bs.BeautifulSoup(body, 'html.parser')
 
-#Setting the title a 2nd time as a method to check if the expected HTML has loaded
-
-print("driver title: ", title)
-
-#currently just testing one piece of data before building out rest of app. 
+#pulling list of search results.
 results = page_parse.find("ul",{"class":"srp-results srp-list clearfix"})
 
+#creating list of the 5 cheapest items
 itemlist = listcheapest(additem(results))
 
-print(itemlist)
+#displaying results
+print("driver title: ", title)
+displaylist(itemlist)
 
 
 
