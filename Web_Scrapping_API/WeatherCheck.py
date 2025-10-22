6300e73b694342163c5258dd4017a188ad7267a8
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-22 14:25:57 +1100

    created function to build a weather item. attmepting to also pull forecast

--- Web_Scrapping_API/WeatherCheck.py
+++ Web_Scrapping_API/WeatherCheck.py
@@ -1,12 +1,29 @@
 import requests
 from ftplib import FTP
 
+def createWeatheritem(item):
+    weatheritem = {}
+    weatheritem['currentTemp'] = int(round(item['main']['temp']))
+    weatheritem['maxTemp'] = int(round(item['main']['temp_max']))
+    weatheritem['minTemp'] = int(round(item['main']['temp_min']))
+    weatheritem['condition'] = item['weather'][0]['description']
+
+    return weatheritem
+
 apiKey = ""
 country = "au"
-city = "Penrith"
+city = "Bargo"
+currenturl = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={apiKey}"
+forecasturl = f"http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&cnt=2&units=metric&APPID={apiKey}"
 
-baseURL = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={apiKey}"
 
-weather = requests.get(baseURL).json()
+# try:
+#     weather = requests.get(currenturl).json()
+#     todayWeather = createWeatheritem(weather)
+#     print(todayWeather)
+# except:
+#     "failed to find the request city or country"
 
+
+weather = requests.get(forecasturl).json()
 print(weather)
