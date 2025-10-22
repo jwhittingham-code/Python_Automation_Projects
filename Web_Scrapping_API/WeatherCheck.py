0568cda7ce847308bdcde2f836218c1f4f55539e
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-22 13:50:54 +1100

    Created API call and capture the requested weather information

--- Web_Scrapping_API/WeatherCheck.py
+++ Web_Scrapping_API/WeatherCheck.py
@@ -1,1 +1,12 @@
-import requests, json
+import requests
+from ftplib import FTP
+
+apiKey = ""
+country = "au"
+city = "Penrith"
+
+baseURL = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={apiKey}"
+
+weather = requests.get(baseURL).json()
+
+print(weather)
