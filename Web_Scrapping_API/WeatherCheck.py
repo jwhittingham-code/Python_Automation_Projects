86879f863e4f68d5bac7e884ba26c7773453fa1e
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-23 15:30:01 +1100

    Created functions for displaying items. added visual polish.

--- Web_Scrapping_API/WeatherCheck.py
+++ Web_Scrapping_API/WeatherCheck.py
@@ -3,6 +3,11 @@ from datetime import date
 
 today = date.today()
 currentday = str(today).split("-")[2]
+apiKey = "-"
+country = "au"
+city = "Penrith"
+currenturl = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={apiKey}"
+forecasturl = f"http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&units=metric&APPID={apiKey}"
 
 def createWeatheritem(item):
     weatheritem = {}
@@ -26,15 +31,35 @@ def createforecast(items):
     
     return weatheritem
 
-apiKey = "-"
-country = "au"
-city = "Bargo"
-currenturl = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={apiKey}"
-forecasturl = f"http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&units=metric&APPID={apiKey}"
+def displayweather(item):
+    try:
+        dateitem = item["date"]
+        current = False
+    except:
+        dateitem = today
+        current = True
+    print(city.upper(), " ", dateitem)
+    if current:
+        print("Current temperature: ", item['currentTemp'])
+        print("Current Conditions: ", item['condition'])
+    else:
+        print("Conditions: ", item['condition'])
+    print("Minium temperature: ", item["minTemp"], "    ", "Maximum temperature: ", item["maxTemp"])
+    print("---------------------------------------------------------")
+    print('')
+
+
 
 weather = createWeatheritem(requests.get(currenturl).json())
 forecast = createforecast(requests.get(forecasturl).json())
 
-print(weather)
+print("---------------------------------------------------------")
+print("------------------------Today----------------------------")
+print("---------------------------------------------------------")
 print()
-print(forecast)
+displayweather(weather)
+print("------------------------forecast-------------------------")
+print("---------------------------------------------------------")
+
+for item in forecast:
+    displayweather(item)
