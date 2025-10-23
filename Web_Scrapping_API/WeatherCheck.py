13cd0a3a90bbdc6db8f0712c41636e5949a2e9f2
jwhittingham-code <jwhittingham002@gmail.com>
2025-10-23 15:09:12 +1100

    figured out the forecast, created a function for forecast object

--- Web_Scrapping_API/WeatherCheck.py
+++ Web_Scrapping_API/WeatherCheck.py
@@ -1,5 +1,8 @@
 import requests
-from ftplib import FTP
+from datetime import date
+
+today = date.today()
+currentday = str(today).split("-")[2]
 
 def createWeatheritem(item):
     weatheritem = {}
@@ -10,20 +13,28 @@ def createWeatheritem(item):
 
     return weatheritem
 
-apiKey = ""
+def createforecast(items):
+    weatheritem = []
+    
+    for item in items['list']:
+        
+        if str(item['dt_txt']).split(" ")[1] == "00:00:00":
+            newitem = createWeatheritem(item)
+            newitem["date"] = str(item["dt_txt"]).split(" ")[0]
+           
+            weatheritem.append(newitem)
+    
+    return weatheritem
+
+apiKey = "-"
 country = "au"
 city = "Bargo"
 currenturl = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID={apiKey}"
-forecasturl = f"http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&cnt=2&units=metric&APPID={apiKey}"
+forecasturl = f"http://api.openweathermap.org/data/2.5/forecast?q={city},{country}&units=metric&APPID={apiKey}"
 
+weather = createWeatheritem(requests.get(currenturl).json())
+forecast = createforecast(requests.get(forecasturl).json())
 
-# try:
-#     weather = requests.get(currenturl).json()
-#     todayWeather = createWeatheritem(weather)
-#     print(todayWeather)
-# except:
-#     "failed to find the request city or country"
-
-
-weather = requests.get(forecasturl).json()
 print(weather)
+print()
+print(forecast)
